fd387c7a1f5e9352381b3cce8f72fade1d3b10b3
vladnovoren <vladnovoren@gmail.com>
2024-09-25 10:19:58 +0300

    To dt

--- src/main/java/ru/mipt/bit/platformer/creatures/RenderableTank.java
+++ src/main/java/ru/mipt/bit/platformer/creatures/RenderableTank.java
@@ -1,4 +1,20 @@
 package ru.mipt.bit.platformer.creatures;
 
-public class RenderableTank {
+import com.badlogic.gdx.graphics.g2d.Batch;
+import ru.mipt.bit.platformer.core.Drawable;
+
+import static ru.mipt.bit.platformer.util.GdxGameUtils.drawTextureRegionUnscaled;
+
+public class RenderableTank implements Drawable {
+    public RenderableTank(Tank tank, TankSkin skin) {
+        this.tank = tank;
+        this.skin = skin;
+    }
+
+    public void draw(Batch batch) {
+        skin.render(batch, tank.getTransform());
+    }
+
+    private Tank tank;
+    private TankSkin skin;
 }
--- src/main/java/ru/mipt/bit/platformer/creatures/Tank.java
+++ src/main/java/ru/mipt/bit/platformer/creatures/Tank.java
@@ -1,31 +1,32 @@
 package ru.mipt.bit.platformer.creatures;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import ru.mipt.bit.platformer.core.Direction;
 import ru.mipt.bit.platformer.core.GameObject;
-import ru.mipt.bit.platformer.core.Renderable;
 import ru.mipt.bit.platformer.util.TileMovement;
 
+import static com.badlogic.gdx.Input.Keys.*;
+import static com.badlogic.gdx.Input.Keys.D;
 import static com.badlogic.gdx.math.MathUtils.isEqual;
 import static ru.mipt.bit.platformer.util.GdxGameUtils.*;
 
 public class Tank extends GameObject {
-    public Tank(float speed) {
+    public Tank(GridPoint2 position, float speed, Rectangle bounds, TileMovement tileMovement) {
+        super(position, 0);
         this.speed = speed;
+        this.bounds = new Rectangle(bounds);
+        requestedDestination = new GridPoint2(position);
+        destination = new GridPoint2(position);
+        this.tileMovement = tileMovement;
     }
 
     @Override
     public void tick(float dt) {
-
+        move(dt, tileMovement);
+        processInput();
     }
-
-//    public Vector2 getDestination() {
-//        return destination;
-//    }
 
     public void move(float dt, TileMovement tileMovement) {
         interpolateMovement(tileMovement);
@@ -35,17 +36,55 @@ public class Tank extends GameObject {
         }
     }
 
-//    public void draw(Batch batch) {
-//        drawTextureRegionUnscaled(batch, textureRegion, bounds, rotation);
-//    }
-
-    private void interpolateMovement(TileMovement tileMovement) {
-        tileMovement.moveRectangleBetweenTileCenters(bounds, position, destination, movementProgress);
+    public GridPoint2 getRequestedDestination() {
+        return requestedDestination.cpy();
     }
 
-    protected Vector2 destination;
+    public void commitRequestedDestination() {
+        destination.set(requestedDestination);
+    }
+
+    private void processInput() {
+        if (Gdx.input.isKeyPressed(UP) || Gdx.input.isKeyPressed(W)) {
+            if (isEqual(movementProgress, 1f)) {
+                requestedDestination.y++;
+                movementProgress = 0f;
+                transform.rotation = Direction.UP.getAngle();
+            }
+        }
+        if (Gdx.input.isKeyPressed(LEFT) || Gdx.input.isKeyPressed(A)) {
+            if (isEqual(movementProgress, 1f)) {
+                requestedDestination.x--;
+                movementProgress = 0f;
+                transform.rotation = Direction.LEFT.getAngle();
+            }
+        }
+        if (Gdx.input.isKeyPressed(DOWN) || Gdx.input.isKeyPressed(S)) {
+            if (isEqual(movementProgress, 1f)) {
+                requestedDestination.y--;
+                movementProgress = 0f;
+                transform.rotation = Direction.DOWN.getAngle();
+            }
+        }
+        if (Gdx.input.isKeyPressed(RIGHT) || Gdx.input.isKeyPressed(D)) {
+            if (isEqual(movementProgress, 1f)) {
+                requestedDestination.x++;
+                movementProgress = 0f;
+                transform.rotation = Direction.RIGHT.getAngle();
+            }
+        }
+    }
+
+    private void interpolateMovement(TileMovement tileMovement) {
+        tileMovement.moveRectangleBetweenTileCenters(bounds, transform.position, destination, movementProgress);
+    }
 
     private float speed;
+    private GridPoint2 requestedDestination;
+    private GridPoint2 destination;
     private float movementProgress = 1.f;
-    private float rotation;
+
+    private Rectangle bounds;
+
+    private TileMovement tileMovement;
 }
